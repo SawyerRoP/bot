32f6527afd42c6e843999650f3d243306fdab0b9
SawyerRoP <netsmut59@gmail.com>
2023-01-17 01:13:34 +0700

    edit notify

--- src/main/java/com/rdcall/bot/GetCall.java
+++ src/main/java/com/rdcall/bot/GetCall.java
@@ -1,9 +1,5 @@
 package com.rdcall.bot;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.PushMessage;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
 import org.jsoup.Jsoup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +10,7 @@ import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.concurrent.ExecutionException;
+
 @RestController
 public class GetCall {
     @GetMapping("/welcome")
@@ -23,9 +19,9 @@ public class GetCall {
         return  now.plusHours(7);
     }
     @PostMapping("/post")
-    public String handleTexMessagePush(@RequestBody String post) throws IOException {
+    public void handleTexMessagePush(@RequestBody String post) throws IOException {
         String decode = URLDecoder.decode(post, StandardCharsets.UTF_8.name());
-        Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type","application/x-www-form-urlencoded").header("Authorization","Bearer 22Zv29T1eE10NPibcQfDsXP7toTE1KPGnZ15K7AondU").ignoreContentType(true).timeout(6000).data("",decode).post();
-        return post;
+        Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type","application/x-www-form-urlencoded").header("Authorization","Bearer 22Zv29T1eE10NPibcQfDsXP7toTE1KPGnZ15K7AondU").ignoreContentType(true).timeout(6000).data("message",decode).post();
+
     }
 }
