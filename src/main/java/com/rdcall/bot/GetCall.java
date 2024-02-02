4ec895374393f9ab3e53347a5e184ec2f9d1ed56
SawyerRoP <netsmut59@gmail.com>
2024-02-02 16:16:21 +0700

    Update GetCall.java

--- src/main/java/com/rdcall/bot/GetCall.java
+++ src/main/java/com/rdcall/bot/GetCall.java
@@ -56,7 +56,7 @@ public class GetCall {
             jdbcTemplate.update("INSERT INTO rdcall (call) VALUES(?)",countCompare);
             log.info(countCompare);
             Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type", "application/x-www-form-urlencoded").header("Authorization", "Bearer L8X6RrMz9WV4UVuq8HOICVqwmRWDzOrILyJt2Gda9BO").ignoreContentType(true).timeout(6000).data("message",decode.toString()).post();
-            Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type", "application/x-www-form-urlencoded").header("Authorization", "Bearer ").ignoreContentType(true).timeout(6000).data("message",decode.toString()).post();
+            Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type", "application/x-www-form-urlencoded").header("Authorization", "Bearer QFMRJH4N7T7T7RrjWebPSGIuxQ6fAL1Zs3fLGhKzPrX").ignoreContentType(true).timeout(6000).data("message",decode.toString()).post();
         }
 
             //Jsoup.connect("https://notify-api.line.me/api/notify").header("Content-Type", "application/x-www-form-urlencoded").header("Authorization", "Bearer ").ignoreContentType(true).timeout(6000).data("message",decode.toString()).post();
